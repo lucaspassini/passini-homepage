77d153e451d30740b3dda23d8d4e730b26509d23
Lucas Cristiano Passini <passini@protonmail.com>
2021-12-24 13:34:04 -0300

    add projects and stack link components

--- components/Footer.js
+++ components/Footer.js
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <div className="mt-12 lg:mt-18 sm:pb-36 sm:py-12 py-6">
       <div className="max-w-4xl px-4 mx-auto text-gray-800 dark:text-white">
-        <div className="pb-8 mb-2 border-t-2 border-gray-600 opacity-25 dark:border-white-300"></div>
+        <div className="pb-8 mb-2 border-t-2 border-gray-600  dark:border-white-300 opacity-20"></div>
         <div className="flex flex-col justify-between lg:flex-row items-center">
           <p>💻 Built with Next.js, Tailwind and Vercel.</p>
           <div className="flex flex-wrap pt-2 sm:space-x-4 space-x-2 font-medium lg:pt-0">
--- components/Navigation.js
+++ components/Navigation.js
@@ -4,8 +4,8 @@ import ThemeSwitch from '../components/ThemeSwitch';
 
 const Navigation = () => {
   return (
-    <div className="sticky top-3 z-20 py-2 bg-opacity-60 bg-transparent md:py-6 md:mb-6 dark:bg-transparent">
-      <div className="container px-4 mx-auto lg:max-w-4xl flex items-center justify-between ">
+    <div className="top-3 z-20 py-2 bg-white md:py-6 md:mb-6 dark:bg-transparent">
+      <div className="container px-4 mx-auto lg:max-w-4xl flex items-center justify-between">
         <Link href="/">
           <a
             className={
@@ -13,6 +13,24 @@ const Navigation = () => {
             }
           >
             /
+          </a>
+        </Link>
+        <Link href="/projects">
+          <a
+            className={
+              'font-light tracking-normal transition-colors text-gray-800 hover:text-sky-500  dark:text-white'
+            }
+          >
+            /projects
+          </a>
+        </Link>
+        <Link href="/stack">
+          <a
+            className={
+              'font-light tracking-normal transition-colors text-gray-800 hover:text-sky-500  dark:text-white'
+            }
+          >
+            /stack
           </a>
         </Link>
         <ThemeSwitch />
